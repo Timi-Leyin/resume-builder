55a354a9748b87a36c6baf018e00e710ab4e2964
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-07-03 23:07:40 +0100

    Removed dynamic template routing in /templates

--- src/Pages/Templates/Templates.tsx
+++ src/Pages/Templates/Templates.tsx
@@ -1,29 +1,18 @@
 import React from 'react'
 import { Panel } from '../Create/Create'
-import * as Template from '../../Components/Resume/Templates'
 import './templates.scss'
 import { Link } from 'react-router-dom'
 const Templates = () => {
-  //   console.log(Template)
- let arr:any[] = [];
-  for(let i of Object.keys(Template)){
-   arr.push(Template[i] as any)
-  // console.log(Template[i])
-  }
   return (
      <section id="resume-create">
        <Panel />
 
        <div className="resume-templates">
-    {
-      arr.map((Temp,i)=>(
 <div>
-<Link to={'/create/' + arr[i].name}  >
-        <Temp key={i}/>
-      </Link>
+<Link to={'/create/Orea'} > Orea </Link>
+<Link to={'/create/Coloful'} > ColorFul </Link>
+<Link to={'/create/Minimal'} > Minimal </Link>
 </div>
-  ))
-    }
        </div>
 
      </section>
