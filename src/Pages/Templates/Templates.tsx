5d7899894bee7ba4f11bc135cc83b46aeb7cac18
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-07-26 19:22:08 +0100

    clear templates

--- src/Pages/Templates/Templates.tsx
+++ src/Pages/Templates/Templates.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 import './templates.scss'
 
 import { Link } from 'react-router-dom';
-
-import orea from '../../Assets/orea.png'
-import niko from '../../Assets/niko.png'
-import niko2 from '../../Assets/niko2.png'
-import blank from '../../Assets/blank.png'
-import minimal from '../../Assets/minimal.png'
 import Panel from '../../Layouts/Panel';
+
+// import orea from '../../Assets/orea.png'
+// import niko from '../../Assets/niko.png'
+// import niko2 from '../../Assets/niko2.png'
+// import blank from '../../Assets/blank.png'
+// import minimal from '../../Assets/minimal.png'
 
 
 const Templates = () => {
@@ -19,11 +19,11 @@ const Templates = () => {
 
        <div className="resume-templates">
 <div>
-<Link to={'/create/orea'} > <img src={orea} alt="Orea" /> </Link>
+{/* <Link to={'/create/orea'} > <img src={orea} alt="Orea" /> </Link>
 <Link to={'/create/niko'} >  <img src={niko} alt="Niko" />  </Link>
 <Link to={'/create/blank'} > <img src={blank} alt="Blank" /> </Link>
 <Link to={'/create/nikoDark'} >  <img src={niko2} alt="Niko2" />  </Link>
-<Link to={'/create/minimal'} >  <img src={minimal} alt="Minimal" />  </Link>
+<Link to={'/create/minimal'} >  <img src={minimal} alt="Minimal" />  </Link> */}
 {/* <Link to={'/create/scape'} >  <img src={} alt="Scape" />  </Link> */}
 </div>
        </div>
