773c89e3b3610db0155687d9fdd34e59110d1e2e
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-08-17 18:30:34 +0100

    added coming soon indicator on template page

--- src/Pages/Templates/Templates.tsx
+++ src/Pages/Templates/Templates.tsx
@@ -13,14 +13,17 @@ const Templates = () => {
 
        <div className="resume-templates">
 <div>
-<Link to={'/create/orea'} > <img src='/images/orea.png' alt="Orea" /> </Link>
-<Link to={'/create/niko'} >  <img src='/images/niko.png' alt="Niko" />  </Link>
-<Link to={'/create/nikoDark'} >  <img src='/images/niko2.png' alt="Niko2" />  </Link>
-<Link to={'/create/minimal'} >  <img src='/images/minimal.png' alt="Minimal" />  </Link>
 <Link to={'/create/scape'} >  <img src='/images/scape.png' alt="Scape" />  </Link>
+<Link to={'/create/nikoDark'} >  <img src='/images/niko2.png' alt="Niko2" />  </Link>
+<Link to={'/create/niko'} >  <img src='/images/niko.png' alt="Niko" />  </Link>
+<Link to={'/create/minimal'} >  <img src='/images/minimal.png' alt="Minimal" />  </Link>
+<Link to={'/create/orea'} > <img src='/images/orea.png' alt="Orea" /> </Link>
 <Link to={'/create/blank'} > <img src='/images/blank.png'  alt="Blank" /> </Link>
 </div>
+       <p className='more'>More Templates Comming Soon ...</p>
+
        </div>
+
 
      </section>
     )
