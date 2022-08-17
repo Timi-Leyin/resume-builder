addcc7a1b34188c7965e2a5e1591b96afabfba93
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-08-17 16:19:18 +0100

    added scape template preview

--- src/Pages/Templates/Templates.tsx
+++ src/Pages/Templates/Templates.tsx
@@ -15,10 +15,10 @@ const Templates = () => {
 <div>
 <Link to={'/create/orea'} > <img src='/images/orea.png' alt="Orea" /> </Link>
 <Link to={'/create/niko'} >  <img src='/images/niko.png' alt="Niko" />  </Link>
-<Link to={'/create/blank'} > <img src='/images/blank.png'  alt="Blank" /> </Link>
 <Link to={'/create/nikoDark'} >  <img src='/images/niko2.png' alt="Niko2" />  </Link>
 <Link to={'/create/minimal'} >  <img src='/images/minimal.png' alt="Minimal" />  </Link>
-<Link to={'/create/scape'} >  <img src='/ima' alt="Scape" />  </Link>
+<Link to={'/create/scape'} >  <img src='/images/scape.png' alt="Scape" />  </Link>
+<Link to={'/create/blank'} > <img src='/images/blank.png'  alt="Blank" /> </Link>
 </div>
        </div>
 
